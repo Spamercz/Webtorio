aac0a2eddf136f98b671289727a2f34323bac712
Spamer <vcevela@gmail.com>
2024-10-19 00:06:48 +0200

    first query

--- src/UI/Presenter/EngineerPresenter.php
+++ src/UI/Presenter/EngineerPresenter.php
@@ -10,4 +10,50 @@ namespace Webtorio\UI\Presenter;
 class EngineerPresenter extends \Nette\Application\UI\Presenter
 {
 
+	public function __construct(
+		private readonly \Spameri\Elastic\ClientProvider $clientProvider,
+	)
+	{
+	}
+
+
+	public function renderDefault()
+	{
+		$this->clientProvider->client()->search([
+			'index' => 'engineer',
+			'body' => [
+				'query' => [
+					'bool' => [
+						'must' => [
+							[
+								'query' => [
+									'bool' => [
+										'should' => [
+											[
+												'multimatch' => [
+													'query' => 'Spamer',
+													'fields' => [
+														'name^3',
+														'description',
+													],
+													'type' => 'best_fields',
+												],
+											],
+											[
+												'term' => [
+													'nickname' => 'Spamer',
+												],
+											],
+											'minimum_should_match' => 1,
+										],
+									],
+								]
+							],
+						],
+					],
+				],
+			],
+		]);
+	}
+
 }
